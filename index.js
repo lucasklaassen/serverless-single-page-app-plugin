e03391c4656fec2c0d7ee905311725388552cd87
lucasklaassen <lucasklaassen1@gmail.com>
2018-03-27 17:27:01 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const spawnSync = require('child_process').spawnSync;
+
+class ServerlessPlugin {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+    this.commands = {
+      syncToS3: {
+        usage: 'Deploys the `app` directory to your bucket',
+        lifecycleEvents: [
+          'sync'
+        ]
+      }
+    };
+
+    this.hooks = {
+      'syncToS3:sync': this.syncDirectory.bind(this)
+    };
+  }
+
+  // syncs the `app` directory to the provided bucket
+  syncDirectory() {
+    const s3Bucket = this.serverless.variables.service.custom.s3Bucket;
+    const s3BucketDirectory = this.serverless.variables.service.custom.s3BucketDirectory;
+    const profile = this.serverless.variables.service.provider.profile;
+    const args = [
+      `--profile=${profile}`,
+      's3',
+      'sync',
+      `${s3BucketDirectory}/`,
+      `s3://${s3Bucket}/`,
+      '--delete'
+    ];
+    const result = spawnSync('aws', args);
+    const stdout = result.stdout.toString();
+    const sterr = result.stderr.toString();
+    if (stdout) {
+      this.serverless.cli.log(stdout);
+    }
+    if (sterr) {
+      this.serverless.cli.log(sterr);
+    }
+    if (!sterr) {
+      this.serverless.cli.log('Successfully synced to the S3 bucket');
+    }
+  }
+}
+
+module.exports = ServerlessPlugin;
